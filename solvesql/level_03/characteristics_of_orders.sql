8e4a13af114a06b2c500019bedd4a6479d229f22
RomkDB <ynrnwa@gmail.com>
2024-04-24 23:32:20 +0900

    240424 solved sovesql level_03

---
+++ solvesql/level_03/characteristics_of_orders.sql
@@ -0,0 +1,32 @@
+/*
+Title : 지역별 주문의 특징
+Content :
+US E-Commerce Records 2020 데이터셋은 미국 이커머스 웹사이트의 판매 데이터 입니다. 
+records 테이블은 주문 번호, 주문 날짜, 주문 지역, 카테고리 등 주문의 상세 정보를 담고 있습니다. 
+이 데이터를 이용하여 미국의 각 지역별로 어떤 카테고리의 상품이 많이 판매되는지 알아보려고 합니다. 
+region, category 별 주문량을 계산해 출력하는 쿼리를 작성해주세요.
+
+결과 데이터 형식
+결과 데이터는 아래와 같은 테이블 형태로 출력되어야 하고, Region 컬럼 기준 오름차순으로 정렬되어 있어야 합니다.
+
+            | Region | Furniture | Office Supplies | Technology |
+| Central |
+| East |
+| South |
+| West |
+
+결과 데이터의 각 컬럼은 다음과 같은 의미를 갖습니다.
+
+- Region - 주문 지역
+- Furniture - 해당 지역 내 가구(’Furniture’) 주문 수
+- Office Supplies - 해당 지역 내 오피스 물품(’Office Supplies’) 주문 수
+- Technology - 해당 지역 내 전자기기(’Technology’) 주문 수
+*/
+
+SELECT region AS Region,
+    COUNT(DISTINCT(CASE WHEN category = 'Furniture' THEN order_id END)) AS Furniture,
+    COUNT(DISTINCT(CASE WHEN category = 'Office Supplies' THEN order_id END)) AS 'Office Supplies',
+    COUNT(DISTINCT(CASE WHEN category = 'Technology' THEN order_id END)) AS Technology
+FROM records
+GROUP BY region
+ORDER BY region;
